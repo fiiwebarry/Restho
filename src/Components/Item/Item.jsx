d7e3b534af8879dc471184b2a3046c8740d8ebff
fiiwebarry <fiiwebarry@yahoo.com>
2023-09-14 18:48:01 +0100

    update

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,13 +1,43 @@
 
 import "./item.css";
+import image1 from "/src/assets/Images/Regular1.png";
+import image2 from "/src/assets/Images/Regular2.png";
+import image3 from "/src/assets/Images/Regular3.png";
+
+import icon from "/src/assets/icon/sub-title-vec.svg";
 
 const Item = () => {
 
 
-    return (<section className="items-tab">
+    return (
+
+        <section className="items-tab">
+
+            <div>
+                <div className="text-flex">
+                    <img src={icon} />
+                    <h3 className="text-into">Our New Item</h3>
+                    <img src={icon} />
+                </div>
+                <div className="res">
+                    <h2 className="Restho">Restho New Item List</h2>
+                </div>
+                <div className="rest">
+                    <p className="Restho-tab">Various versions have evolved over the years, sometimes on purpose.</p>
+                </div>
+            </div>
+
+            <div className="container maper">
+
+                <img src={image1} />
+                <img src={image2} />
+                <img src={image3} />
+            </div>
 
 
-    </section>)
+
+
+        </section>)
 
 }
 export default Item;
